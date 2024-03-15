c513471734a3b5af7e019a615dbf6e36413fad14
DESKTOP-9HSRLI7\admin <tejaswibhokare17@gmail.com>
2024-03-14 23:27:17 -0400

    Added FoodDetail component and define routes/paths in routes component.

---
+++ tinytastehub/src/Component/FoodDetail.jsx
@@ -0,0 +1,20 @@
+import Header from "./Header";
+
+export default function FoodDetail() {
+    return (
+        <>
+            <Header />
+            <img src='/img/FoodHome.jpg'
+                alt="service"
+                width='100%'
+                height='155%'
+                className='imgBack'
+            />
+            <div>
+                <h3>EGG</h3>
+
+            </div>
+        </>
+
+    )
+}
--- tinytastehub/src/Component/Routes.jsx
+++ tinytastehub/src/Component/Routes.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainPage from "./Home";;
+import MainPage from "./Home";
+import FoodDetail from "./FoodDetail";
+import Guides from "./Guides";
+
 function Router() {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <MainPage />
+        },
+        {
+            path: "/FoodDetail",
+            element: <FoodDetail />
+        },
+        {
+            path: "/guides",
+            element: <Guides />
         },
 
     ])
